5233ff97b91b335e7ce6323f45706465d7b0e26a
devpelux <info@devpelux.xyz>
2021-12-28 22:38:13 +0100

    Added ColorExtensions, SizeExtensions, StringExtensions.

---
+++ CoreTools/Extensions/SizeExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Drawing;
+
+namespace CoreTools.Extensions
+{
+    /// <summary>
+    /// Provides a set of <see cref="Size"/> extensions.
+    /// </summary>
+    public static class SizeExtensions
+    {
+        /// <summary>
+        /// Invert the <see cref="Size"/> by replacing every value with its negative.
+        /// </summary>
+        /// <param name="size">Current <see cref="Size"/>.</param>
+        /// <returns></returns>
+        public static Size Invert(this Size size) => new(-size.Width, -size.Height);
+
+        /// <summary>
+        /// Initializes a new <see cref="Size"/> with uniform dimensions.
+        /// </summary>
+        /// <param name="dim">Dimensions.</param>
+        /// <returns>New <see cref="Size"/> with uniform dimensions.</returns>
+        public static Size UniformSize(int dim) => new(dim, dim);
+    }
+}
