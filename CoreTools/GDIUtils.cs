28c8d34486199d16cbd83df706b13d701fea95ba
devpelux <info@devpelux.xyz>
2021-12-29 22:36:37 +0100

    Moved all the new methods added into GraphicUtils into GDIUtils, updated changelog.

---
+++ CoreTools/GDIUtils.cs
@@ -0,0 +1,60 @@
+﻿using CoreTools.Core;
+using System.Drawing;
+using System.Runtime.Versioning;
+
+namespace CoreTools
+{
+    /// <summary>
+    /// Provides a set of utilities for handling some gdi operations.
+    /// </summary>
+    public static class GDIUtils
+    {
+        /// <summary>
+        /// Returns the color of the pixel at a specified position.
+        /// </summary>
+        /// <param name="pos">Position of the pixel from to get the color.</param>
+        /// <param name="takeScreenshotWhenPossible">Take a screenshot when possible to get the pixel color
+        /// (this may be slower and is supported only in Windows).</param>
+        /// <returns>Color of the pixel at the specified position.</returns>
+        public static Color GetPixelColor(Point pos, bool takeScreenshotWhenPossible = false)
+            => InternalMethods.GetPixelColor(pos, takeScreenshotWhenPossible);
+
+        /// <summary>
+        /// Returns the color of the pixel at the current cursor position on display.
+        /// </summary>
+        /// <param name="takeScreenshotWhenPossible">Take a screenshot when possible to get the pixel color
+        /// (this may be slower and is supported only in Windows).</param>
+        /// <returns>Color of the pixel at the current cursor position on display.</returns>
+        public static Color GetPixelColorAtCursorPos(bool takeScreenshotWhenPossible = false)
+            => GetPixelColor(GraphicUtils.GetCursorPos(), takeScreenshotWhenPossible);
+
+        /// <summary>
+        /// Captures a screenshot from a specified position, with a specified size.
+        /// </summary>
+        /// <param name="pos">Top-left corner of the screenshot.</param>
+        /// <param name="size">Size of the screenshot.</param>
+        /// <returns>Screenshot as a <see cref="Bitmap"/>.</returns>
+        [SupportedOSPlatform("windows")]
+        public static Bitmap CaptureScreenshot(Point pos, Size size)
+            => InternalMethods.CaptureScreenshot(pos, size);
+
+        /// <summary>
+        /// Captures a screenshot from the current cursor position on display, with a specified size, and offset.
+        /// </summary>
+        /// <param name="size">Size of the screenshot.</param>
+        /// <param name="offset">Offset of the screenshot.</param>
+        /// <returns>Screenshot as a <see cref="Bitmap"/>.</returns>
+        [SupportedOSPlatform("windows")]
+        public static Bitmap CaptureScreenshotAtCursorPos(Size size, Size offset)
+            => CaptureScreenshot(GraphicUtils.GetCursorPos() + offset, size);
+
+        /// <summary>
+        /// Captures a screenshot from the current cursor position on display, with a specified size.
+        /// </summary>
+        /// <param name="size">Size of the screenshot.</param>
+        /// <returns>Screenshot as a <see cref="Bitmap"/>.</returns>
+        [SupportedOSPlatform("windows")]
+        public static Bitmap CaptureScreenshotAtCursorPos(Size size)
+            => CaptureScreenshotAtCursorPos(size, Size.Empty);
+    }
+}
---
+++ CoreToolsTest/GDIUtilsTests.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+using System.Drawing;
+using System.Runtime.Versioning;
+
+namespace CoreToolsTest
+{
+    [TestClass]
+    public class GDIUtilsTests
+    {
+        [TestMethod]
+        public void GetPixelColorAtCursorPosR()
+        {
+            Point pos = CoreTools.GraphicUtils.GetCursorPos();
+            Debug.WriteLine(pos);
+            Color color = CoreTools.GDIUtils.GetPixelColorAtCursorPos();
+            Color testColor = Color.FromArgb(255, 0, 0); //FFFF0000
+            Debug.WriteLine("Color getted: " + color);
+            Debug.WriteLine("Test color: " + testColor);
+            Assert.AreEqual(testColor, color);
+        }
+
+        [TestMethod]
+        public void GetPixelColorAtCursorPosG()
+        {
+            Point pos = CoreTools.GraphicUtils.GetCursorPos();
+            Debug.WriteLine(pos);
+            Color color = CoreTools.GDIUtils.GetPixelColorAtCursorPos();
+            Color testColor = Color.FromArgb(0, 255, 0); //FF00FF00
+            Debug.WriteLine("Color getted: " + color);
+            Debug.WriteLine("Test color: " + testColor);
+            Assert.AreEqual(testColor, color);
+        }
+
+        [TestMethod]
+        public void GetPixelColorAtCursorPosB()
+        {
+            Point pos = CoreTools.GraphicUtils.GetCursorPos();
+            Debug.WriteLine(pos);
+            Color color = CoreTools.GDIUtils.GetPixelColorAtCursorPos();
+            Color testColor = Color.FromArgb(0, 0, 255); //FF0000FF
+            Debug.WriteLine("Color getted: " + color);
+            Debug.WriteLine("Test color: " + testColor);
+            Assert.AreEqual(testColor, color);
+        }
+
+        [TestMethod]
+        [SupportedOSPlatform("windows")]
+        public void GetPixelColorAtCursorPosR2()
+        {
+            Point pos = CoreTools.GraphicUtils.GetCursorPos();
+            Debug.WriteLine(pos);
+            Color color = CoreTools.GDIUtils.GetPixelColorAtCursorPos(true);
+            Color testColor = Color.FromArgb(255, 0, 0); //FFFF0000
+            Debug.WriteLine("Color getted: " + color);
+            Debug.WriteLine("Test color: " + testColor);
+            Assert.AreEqual(testColor, color);
+        }
+
+        [TestMethod]
+        [SupportedOSPlatform("windows")]
+        public void GetPixelColorAtCursorPosG2()
+        {
+            Point pos = CoreTools.GraphicUtils.GetCursorPos();
+            Debug.WriteLine(pos);
+            Color color = CoreTools.GDIUtils.GetPixelColorAtCursorPos(true);
+            Color testColor = Color.FromArgb(0, 255, 0); //FF00FF00
+            Debug.WriteLine("Color getted: " + color);
+            Debug.WriteLine("Test color: " + testColor);
+            Assert.AreEqual(testColor, color);
+        }
+
+        [TestMethod]
+        [SupportedOSPlatform("windows")]
+        public void GetPixelColorAtCursorPosB2()
+        {
+            Point pos = CoreTools.GraphicUtils.GetCursorPos();
+            Debug.WriteLine(pos);
+            Color color = CoreTools.GDIUtils.GetPixelColorAtCursorPos(true);
+            Color testColor = Color.FromArgb(0, 0, 255); //FF0000FF
+            Debug.WriteLine("Color getted: " + color);
+            Debug.WriteLine("Test color: " + testColor);
+            Assert.AreEqual(testColor, color);
+        }
+
+        [TestMethod]
+        [SupportedOSPlatform("windows")]
+        public void CaptureScreenshotAtCursorPos()
+        {
+            Bitmap bitmap = CoreTools.GDIUtils.CaptureScreenshotAtCursorPos(new Size(2, 8));
+            for (int i = 0; i < bitmap.Width; i++)
+            {
+                for (int j = 0; j < bitmap.Height; j++)
+                {
+                    Debug.WriteLine($"Color ({i},{j}): " + bitmap.GetPixel(i, j));
+                }
+            }
+        }
+
+        [TestMethod]
+        [SupportedOSPlatform("windows")]
+        public void CaptureScreenshotAtCursorPosWithOffset()
+        {
+            Bitmap bitmap = CoreTools.GDIUtils.CaptureScreenshotAtCursorPos(new Size(2, 8), new Size(10, 0));
+            for (int i = 0; i < bitmap.Width; i++)
+            {
+                for (int j = 0; j < bitmap.Height; j++)
+                {
+                    Debug.WriteLine($"Color ({i},{j}): " + bitmap.GetPixel(i, j));
+                }
+            }
+        }
+    }
+}
